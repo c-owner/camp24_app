f01ba5f3f018c460f6ebd7b21c10ffc8717f9782
corner <corner3499@gmail.com>
2022-09-29 10:41:39 +0900

    user page ++

---
+++ pages/user/findid.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<!--        아이디 찾기 페이지-->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FindIdPage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/user/findpw.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<!--        비번찾기 페이지-->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FindPwPage"
+}
+</script>
+
+<style scoped>
+
+</style>
