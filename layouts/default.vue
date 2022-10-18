1fc1a7bc334c9aed294351f43d2b30da63b43872
corner <corner3499@gmail.com>
2022-10-19 00:02:24 +0900

    register

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,19 @@
 <template>
     <div id="app" class="safe-top safe-left safe-right safe-bottom mx-4">
         <div class="disable-scrollbars max-h-screen h-full overflow-y-scroll">
-
-
-            <div class="app-header flex">
-                <div class="text-left">
-                    <button @click="routerBack()" v-if="$route.path !== '/'"
-                        class="ph-arrow-bend-down-left btn p-1 rounded bg-white"></button>
-                </div>
-            </div>
             <div class="app-content">
+                <div class="app-header flex">
+                    <div class="text-left">
+                        <button @click="routerBack()" v-if="$route.path !== '/'"
+                            class="ph-arrow-bend-down-left btn p-1 rounded bg-white"></button>
+                    </div>
+                </div>
                 <Nuxt />
-            </div>
-            <div class="app-footer flex bg-dark text-white space-between">
-                <button class="btn text-white"><i class="ph-magnifying-glass ft24" /></button>
-                <button class="btn text-white" @click="moveUrl('/')"><i class="ph-house ft24" /></button>
-                <button class="btn text-white" @click="moveUrl('/user/mypage')"><i class="ph-user ft24" /></button>
+                <div class="app-footer flex bg-dark text-white space-between">
+                    <button class="btn text-white"><i class="ph-magnifying-glass ft24" /></button>
+                    <button class="btn text-white" @click="moveUrl('/')"><i class="ph-house ft24" /></button>
+                    <button class="btn text-white" @click="moveUrl('/user/mypage')"><i class="ph-user ft24" /></button>
+                </div>
             </div>
         </div>
     </div>
@@ -55,11 +53,16 @@ export default {
 }
 .app-content {
     height: 100vh;
+    position: relative;
+    padding-bottom: 70px;
 }
 .app-footer {
-    height: 80px;
-    padding: 0 24px;
-    position: sticky;
+    padding: 1em;
+    position: absolute;
     bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 70px;
+
 }
 </style>
--- pages/user/register.vue
+++ pages/user/register.vue
@@ -11,10 +11,18 @@
                             </h6>
                         </div>
                         <div class="btn-wrapper text-center">
-                            <button class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
-                                <img alt="github" class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/github.svg">Github</button>
-                            <button class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150" type="button">
-                                <img alt="google" class="w-5 mr-1" src="https://demos.creative-tim.com/notus-js/assets/img/google.svg">Google </button>
+                            <button
+                                class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                                type="button">
+                                <img alt="github" class="w-5 mr-1"
+                                     src="https://demos.creative-tim.com/notus-js/assets/img/github.svg">Github
+                            </button>
+                            <button
+                                class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                                type="button">
+                                <img alt="google" class="w-5 mr-1"
+                                     src="https://demos.creative-tim.com/notus-js/assets/img/google.svg">Google
+                            </button>
 
                         </div>
                         <hr class="mt-6 border-b-1 border-blueGray-300">
@@ -25,16 +33,32 @@
                         </div>
                         <form>
                             <div class="relative w-full mb-3">
-                                <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">Email</label><input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Email">
+                                <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                       for="grid-password">Email</label>
+                                <input type="email"
+                                       class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                       placeholder="Email"/>
                             </div>
                             <div class="relative w-full mb-3">
-                                <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">Password</label><input type="password" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Password">
+                                <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                       for="grid-password">Password</label>
+                                <input type="password"
+                                       class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                       placeholder="Password"/>
                             </div>
                             <div>
-                                <label class="inline-flex items-center cursor-pointer"><input id="customCheckLogin" type="checkbox" class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"><span class="ml-2 text-sm font-semibold text-blueGray-600">Remember me</span></label>
+                                <label class="inline-flex items-center cursor-pointer">
+                                    <input id="customCheckLogin"
+                                           type="checkbox"
+                                           class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150">
+                                    <span class="ml-2 text-sm font-semibold text-blueGray-600">Remember me</span>
+                                </label>
                             </div>
                             <div class="text-center mt-6">
-                                <button class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button"> Sign In </button>
+                                <button
+                                    class="btn"
+                                    type="button"> Sign In
+                                </button>
                             </div>
                         </form>
                     </div>
